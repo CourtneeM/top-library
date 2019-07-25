0cc9dad566b885057ed34e15617e10383d937826
Courtnee M <courtneemeaghan@gmail.com>
2019-07-24 20:08:31 -0500

    Added remove button functionality.

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
 const bookShelf = document.querySelector('main');
 const btnNewBook = document.querySelector('#btn-new');
 const newBookForm = document.querySelector('form');
-var myLibrary = ['Book 1', 'Book 2', 'Book 3'];
+const btnAddBook = document.querySelector('#add-book');
+const btnRemoveBook = document.querySelector('.btnRemove');
+var myLibrary = [];
 
 function Book() {
 
 }
 
-function addBookToLibrary() {
-  let newBook = prompt("What book would you like to add to your library?");
-  myLibrary.push(newBook);
+function addBookToLibrary(title) {
+  myLibrary.push(title);
+  render();
 }
 
 function render() {
   let numBooks = myLibrary.length
+  let bookCard = document.createElement('div');
+  let book = document.createElement('p');
+  let removeBook = document.createElement('button');
+  removeBook.textContent = "Delete";
+  removeBook.classList.add('btnRemove');
   for(let i = 0; i < numBooks; i++) {
-    let bookCard = document.createElement('div');
-    let book = document.createElement('p');
     bookShelf.appendChild(bookCard);
     book.textContent = myLibrary[i];
     bookCard.appendChild(book);
+    bookCard.appendChild(removeBook);
   }
+  console.log(myLibrary);
 }
 
-btnNewBook.addEventListener('click', function(e) {
-  newBookForm.style.display = "flex";
-});
+function eventHandler() {
+  document.addEventListener('click', function(e) {
+    if(e.target.textContent === "New Book") {
+      newBookForm.style.display = "flex";
+      btnAddBook.style.display = "block";
+    }
 
-render();
+    if(e.target.textContent === "Add") {
+      const bookTitle = document.querySelector('#title');
+      const bookAuthor = document.querySelector('#author');
+      const numPages = document.querySelector('#pages');
+      const hasReadBox = document.querySelector('input[type="checkbox"]');
+      let hasRead;
+      if(hasReadBox.checked) {
+        hasRead = "yes";
+      } else {
+        hasRead = "no";
+      }
+      addBookToLibrary(bookTitle.value);
+      bookTitle.value = "";
+      newBookForm.style.display = "none";
+      btnAddBook.style.display = "none";
+    }
+
+    if(e.target.textContent === "Delete") {
+      let bookToDelete = e.target.parentNode.children[0].textContent;
+      for(let i = 0; i < myLibrary.length; i++) {
+        if(bookToDelete === myLibrary[i]) {
+          myLibrary.splice(i, 1);
+        }
+      }
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }
+  })
+}
+
+eventHandler();
